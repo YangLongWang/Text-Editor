404cd0f376b2e543bbc8b252429f9586abbc34ba
yang <joey674901205@gmail.com>
2022-09-09 18:06:07 -0400

    fixed getDB problem, now can show content

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,6 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   butInstall.style.visibility = 'visible';
@@ -12,15 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.textContent = 'Installed!';
   });
 });
-// TODO: Implement a click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async (event) => {
-//   event.prompt();
-//   butInstall.setAttribute('disabled', true);
-//   butInstall.textContent = 'Installed!';
-// });
 
-
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   console.log('👍', 'appinstalled', event);
 });
